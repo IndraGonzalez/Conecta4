b72ba1c5cd01861a92251be9ee4aad3e837a1b98
Indra González Cabrera <indra.glez@gmail.com>
2016-04-29 12:52:32 +0100

    Versión 3
    Ya tenemos hecha toda la interfaz (de la versión anterior)
    Ya recorremos el tablero, ahora hay que implementar las heurísticas.

--- heuristics.py
+++ heuristics.py
@@ -1,8 +1,24 @@
 import random
 
-
-def h0(self):
-    return random.random()
+def h0(state):
+    board = state.board.copy()
+    x=1
+    y=1
+    v=6
+    h=7
+    while (y<=v) and (x<=h):
+        celda = board.get((x, y))
+        # Hay que pasar cuál es el jugador e ir mirando las celdas a su alrededor
+        #FACIL = horizontal y vertical
+        #MEDIO = diagonales también
+        #DIFICIL = lo anterior y que intente ganar
+        if(celda=='X'):
+            h +=1
+        y += y
+        x += x
+    h = random.random()
+    print h
+    return h
 
 def h1(self):
     return random.random()
